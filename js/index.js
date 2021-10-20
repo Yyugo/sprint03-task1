d4af758c87d4e8a31dd694294ae6623eb0e8d825
Yura <yurgayura44@gmail.com>
2021-10-20 21:04:25 +0300

    Incorrect order of elements

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// DOM Document Object Model
+
+//User 1 
+const listUsers00 = document.getElementById('list').children[0].innerText;
+console.log(listUsers00);
+
+//User 2 
+const listUsers04 = document.getElementById('list').children[4].innerText;
+console.log(listUsers04);
+
+//User 3 
+const listUsers01 = document.getElementById('list').children[1].innerText;
+console.log(listUsers01);
+
+//User 4 
+const listUsers03 = document.getElementById('list').children[3].innerText;
+console.log(listUsers03);
+
+//User 5 
+const listUsers02 = document.getElementById('list').children[2].innerText;
+console.log(listUsers02);
